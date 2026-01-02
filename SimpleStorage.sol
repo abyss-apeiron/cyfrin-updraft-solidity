3e9f1e062f672bf13670be08a2334eef8cb92821
Joel Orukpe <joel.orukpe@eng.uniben.edu>
2026-01-02 20:52:55 +0100

    Implement store and retrieve functions in SimpleStorage
    Added functions to store and retrieve a favorite number.

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -2,5 +2,19 @@
 pragma solidity 0.8.18; // this is the solidity version
 
 contract SimpleStorage {
+    // Basic Types: boolean, uint, int, address, bytes
     
+    uint256 public favouriteNumber; // 0
+
+    function store(uint256 _favouriteNumber) public {
+        favouriteNumber = _favouriteNumber;
+    }
+
+    function retrieve() public view returns(uint256) {
+        return favouriteNumber;
+    }
+
+    // view reads state from the blockchain but doesn't modify the state
+    // pure does not read nor update the state of the blockchain
+
 }
