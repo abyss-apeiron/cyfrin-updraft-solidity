14567a846f1badd82821f339cba0b8b19a13f121
Joel Orukpe <joel.orukpe@eng.uniben.edu>
2026-01-03 21:58:56 +0100

    Refactor SimpleStorage to use myfavouriteNumber
    Added an array and struct to SimpleStorage

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -4,17 +4,30 @@ pragma solidity 0.8.18; // this is the solidity version
 contract SimpleStorage {
     // Basic Types: boolean, uint, int, address, bytes
     
-    uint256 public favouriteNumber; // 0
+    uint256 myfavouriteNumber; // 0
+
+    // uint256[] listOfFavouriteNumbers; // [0, 78, 90]
+    struct Person{
+        uint256 favouriteNumber;
+        string name;
+    }
+
+    // dynamic array due to the [] having nothing inside meaning it can have any size
+    Person[] public listOfPeople;
+
 
     function store(uint256 _favouriteNumber) public {
-        favouriteNumber = _favouriteNumber;
+        myfavouriteNumber = _favouriteNumber;
     }
 
     function retrieve() public view returns(uint256) {
-        return favouriteNumber;
+        return myfavouriteNumber;
     }
 
     // view reads state from the blockchain but doesn't modify the state
     // pure does not read nor update the state of the blockchain
 
+    function addPerson(string memory _name, uint256 _favouriteNumber) public {
+        listOfPeople.push(Person(_favouriteNumber, _name));
+    }
 }
