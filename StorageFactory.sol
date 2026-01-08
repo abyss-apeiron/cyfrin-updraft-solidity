b7bae221202bd933fd14548df0a3d265e98816e7
Joel Orukpe <joel.orukpe@eng.uniben.edu>
2026-01-08 23:02:58 +0100

    Refactor StorageFactory by removing SimpleStorage
    Removed SimpleStorage contract and its related functions. Updated StorageFactory to import SimpleStorage allowing for more efficient code.

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -2,49 +2,12 @@
 
 pragma solidity ^0.8.18;
 
-contract SimpleStorage {
-    // Basic Types: boolean, uint, int, address, bytes
-    
-    uint256 myfavouriteNumber; // 0
-
-    // uint256[] listOfFavouriteNumbers; // [0, 78, 90]
-    struct Person{
-        uint256 favouriteNumber;
-        string name;
-    }
-
-    // dynamic array due to the [] having nothing inside meaning it can have any size
-    Person[] public listOfPeople;
-
-    mapping(string => uint256) public nameToFavouriteNumber;
-
-
-    function store(uint256 _favouriteNumber) public {
-        myfavouriteNumber = _favouriteNumber;
-    }
-
-    function retrieve() public view returns(uint256) {
-        return myfavouriteNumber;
-    }
-
-    // view reads state from the blockchain but doesn't modify the state
-    // pure does not read nor update the state of the blockchain
-
-    // calldata & memory refer to a temporary storage but while memory variables are modifiable, calldata variables are not modifiable
-    // storage refers to permanent storage that can be modified. state variables are of this type
-    function addPerson(string memory _name, uint256 _favouriteNumber) public {
-        listOfPeople.push(Person(_favouriteNumber, _name));
-        nameToFavouriteNumber[_name] = _favouriteNumber;
-    }
-}
+import {SimpleStorage, SimpleStorage2} from "./SimpleStorage.sol";
 
 contract StorageFactory {
-
     SimpleStorage public simpleStorage;
 
     function createSimpleStorageContract() public {
         simpleStorage = new SimpleStorage();
-
     }
-
 }
