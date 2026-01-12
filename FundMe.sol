9d1d11ac5497cd84d963c0fa978dc3ffef41f5fc
Joel Orukpe <joel.orukpe@eng.uniben.edu>
2026-01-12 21:05:19 +0100

    Update funding requirement to minimumUsd value

--- FundMe.sol
+++ FundMe.sol
@@ -6,13 +6,35 @@
 
 pragma solidity ^0.8.18;
 
-contract FundMe{
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+contract FundMe {
+
+    uint256 public minimumUsd = 5;
+    
     function fund() public payable {
         // Allow users to send $
-        // Have a minimum $ sent
-        require(msg.value > 1e18, "You need to spend more ETH!"); // 1 Eth = 1 * 10 ** 18 Wei
+        // Have a minimum $ sent $5
+        require(msg.value >= minimumUsd, "You need to spend more ETH!"); // 1 Eth = 1 * 10 ** 18 Wei
+
+
+        // What is a revert?
+        // Undo any actions that have been done, and send the remaining gas back
+
+
     }
 
     // function withdraw() public {}
+
+    function getPrice() public {
+        // Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        // ABI
+    }
+    function getConversionRate() public {
+
+    }
+    function getVersion() public view returns (uint256) {
+
+    }
 
 }
